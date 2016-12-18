b9a8e2af26bee7aaa55a2a74e108d3250a183717
oricalvo <ori.calvo@gmail.com>
2016-12-18 16:57:07 +0200

    More

---
+++ configureWebProxy.cmd
@@ -0,0 +1,4 @@
+CALL npm config set proxy http://16.82.112.30:8080/
+CALL npm config set https-proxy http://16.82.112.30:8080/
+CALL git config --global http.proxy http://web-proxy.isr.hp.com:8080/
+CALL git config --global https.proxy http://web-proxy.isr.hp.com:8080/
