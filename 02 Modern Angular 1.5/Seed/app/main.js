91dc65351ed2889a046fde717e5316146395bea9
oricalvo <ori.calvo@gmail.com>
2016-12-28 09:35:24 +0200

    More

--- 02 Modern Angular 1.5/Seed/app/main.js
+++ 02 Modern Angular 1.5/Seed/app/main.js
@@ -1,6 +1,6 @@
 "use strict";
 var angular = require("angular");
-var app_component_1 = require("./app.component");
+var app_component_1 = require("./components/app.component");
 var app_module_1 = require("./app.module");
 app_component_1.AppComponent;
 angular.bootstrap(document.getElementById("html"), [app_module_1.appModule.name]);
--- 02 Modern Angular 1.5/Seed/app/main.ts
+++ 02 Modern Angular 1.5/Seed/app/main.ts
@@ -1,5 +1,5 @@
 import * as angular from "angular";
-import {AppComponent} from "./app.component";
+import {AppComponent} from "./components/app.component";
 import {appModule} from "./app.module";
 
 AppComponent;
